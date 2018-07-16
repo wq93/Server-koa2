1dca33d4f6c5756341668af42cb4368daabde1fc
wq93 <214180117@qq.com>
2018-07-16 14:44:58 +0800

    实现异步的componse

--- test.js
+++ test.js
@@ -1,26 +1,73 @@
-function add(x, y) {
-  return x + y
+async function fn1(next) {
+  console.log('fn1')
+  await next()
+  console.log('end fn1')
 }
 
-function double(z) {
-  return z * 2
+async function fn2(next) {
+  console.log('fn2')
+  await delay()
+  await next()
+  console.log('end fn2')
 }
 
-const middllewares = [add, double]
-let len = middllewares.length
+function fn3() {
+  console.log('fn3')
+}
 
-function componse(midds) {
-  return (...args) => {
-    // 初始值
-    let res = midds[0](...args)
-    for (let i = 1; i < len; i++) {
-      res = midds[i](res)
+// 等待
+function delay() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    },2000)
+  })
+}
+
+function compose(middlewares) {
+  return function () {
+    return dispatch(0)
+
+    function dispatch(i) {
+      let fn = middlewares[i]
+      if (!fn) {
+        return Promise.resolve()
+      }
+      return Promise.resolve(fn(function next() {
+        return dispatch(i + 1)
+      }))
     }
-    return res
   }
 }
 
-const fn = componse(middllewares)
-const res = fn(1,2)
+const middlewares = [fn1, fn2, fn3]
+const finalFn = compose(middlewares)
+finalFn()
 
-console.log(res)
+
+// function add(x, y) {
+//   return x + y
+// }
+//
+// function double(z) {
+//   return z * 2
+// }
+//
+// const middllewares = [add, double]
+// let len = middllewares.length
+//
+// function componse(midds) {
+//   return (...args) => {
+//     // 初始值
+//     let res = midds[0](...args)
+//     for (let i = 1; i < len; i++) {
+//       res = midds[i](res)
+//     }
+//     return res
+//   }
+// }
+//
+// const fn = componse(middllewares)
+// const res = fn(1,2)
+//
+// console.log(res)
